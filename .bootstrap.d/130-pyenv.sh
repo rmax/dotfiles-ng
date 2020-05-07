25f3b31fc053ea3d775e25aa074ee10fa824d998
R. Max Espinoza <me@rmax.io>
2020-05-07 18:34:50 +0200

    sync

--- .bootstrap.d/130-pyenv.sh
+++ .bootstrap.d/130-pyenv.sh
@@ -1,17 +1,22 @@
-
-PYVER_LEGACY="3.6.10"
-PYVER_STABLE="3.7.7"
-PYVER_EDGE="3.8.2"
-
 PYVER_BASE_PACKAGES="pip wheel"
 PYVER_EXTRA_PACKAGES="pipenv ipython"
 
-for PYVER in $PYVER_LEGACY $PYVER_STABLE $PYVER_EDGE; do
+function pip-install() {
+  pyenv exec pip install --upgrade $1
+}
+
+function pyver-install() {
+  PYVER=$1
   pyenv install --skip-existing --verbose $PYVER
   pyenv local $PYVER
-  pyenv exec pip install --upgrade $PYVER_BASE_PACKAGES
-  pyenv exec pip install --upgrade $PYVER_EXTRA_PACKAGES
-done
+  pip-install $PYVER_BASE_PACKAGES
+  pip-install $PYVER_EXTRA_PACKAGES
+}
 
-pyenv global $PYVER_STABLE
-pyenv local $PYVER_STABLE
+pyver-install 3.6.10
+pyver-install 3.7.7
+pyver-install 3.8.2
+pyver-install pypy-5.7.1
+
+pyenv global 3.7.7
+pyenv local 3.7.7
